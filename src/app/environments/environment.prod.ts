cc5a21e8da603a8ec1eab4f6e272c3a8bb95b321
Carlos Morales <CrlsMrls@outlook.com>
2024-05-26 22:40:42 +0200

    Add Firebase SDK

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  // Web app's Firebase configuration
+  firebaseConfig: {
+    apiKey: 'AIzaSyCXEAsw45mQ1ByJuvH19WTbqgxIlwydh38',
+    authDomain: 'ai-playground-424107.firebaseapp.com',
+    projectId: 'ai-playground-424107',
+    storageBucket: 'ai-playground-424107.appspot.com',
+    messagingSenderId: '219236925850',
+    appId: '1:219236925850:web:70cf37726e5f685914fa23',
+  },
+};
